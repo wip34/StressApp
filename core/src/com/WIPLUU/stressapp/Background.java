2cc3136f7082e0923626ed99e4776a402e5487c5
wip34 <hanlinip@yahoo.com>
2015-11-03 14:27:18 -0600

    back
    overwrite albert's code

---
+++ core/src/com/WIPLUU/stressapp/Background.java
@@ -0,0 +1,17 @@
+package com.WIPLUU.stressapp;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+/**
+ * Created by rakion3498 on 11/3/2015.
+ */
+public class Background {
+    private Sprite back;
+
+    Background(Sprite b) {
+        back = new Sprite(b);
+    }
+
+
+
+}

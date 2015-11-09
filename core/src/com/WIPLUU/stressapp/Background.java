fd1f43223a1c00c9d2d66ef15557d905ba98ce10
AlbertLuu <AlbertLuu42@gmail.com>
2015-11-09 13:46:03 -0600

    Background
    Created base background and added image

--- core/src/com/WIPLUU/stressapp/Background.java
+++ core/src/com/WIPLUU/stressapp/Background.java
@@ -1,17 +1,23 @@
 package com.WIPLUU.stressapp;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 /**
  * Created by rakion3498 on 11/3/2015.
  */
 public class Background {
-    private Sprite back;
+    private Texture back;
 
-    Background(Sprite b) {
-        back = new Sprite(b);
+    Background(Texture t) {
+        back = t;
     }
 
+    public void draw(SpriteBatch b)
+    {
+        b.draw(back, 0, 0);
+    }
 
 
 }
--- core/src/com/WIPLUU/stressapp/MyGdxGame.java
+++ core/src/com/WIPLUU/stressapp/MyGdxGame.java
@@ -4,16 +4,19 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MyGdxGame extends ApplicationAdapter {
+
 	SpriteBatch batch;
 	Texture img;
+	Background test;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("johncena.jpg");
+		Background test = new Background(img);
 	}
 
 	@Override
@@ -21,7 +24,7 @@ public class MyGdxGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		test.draw(batch);
 		batch.end();
 		
 	}

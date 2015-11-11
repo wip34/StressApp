0c0abdb11723999bbf50983fe2f51d9c27b6a6f2
AlbertLuu <AlbertLuu42@gmail.com>
2015-11-11 14:34:10 -0600

    Picture Array
    Created a class to make arrays to store pictures.

---
+++ core/src/com/WIPLUU/stressapp/PicArray.java
@@ -0,0 +1,16 @@
+package com.WIPLUU.stressapp;
+
+import com.badlogic.gdx.graphics.Texture;
+
+import java.util.ArrayList;
+
+/**
+ * Created by luu289714 on 11/11/2015.
+ */
+public class PicArray {
+    ArrayList<Texture> pics;
+
+    PicArray(ArrayList<Texture> a) {
+        a = new ArrayList<Texture>();
+    }
+}

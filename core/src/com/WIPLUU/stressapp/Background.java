f6c72090b349a393f85c73f6a4ff552bfcfabf85
wip34 <hanlinip@yahoo.com>
2015-12-04 14:07:39 -0600

    First Atlas for Buttons
    random colored buttons to access

--- core/src/com/WIPLUU/stressapp/Background.java
+++ core/src/com/WIPLUU/stressapp/Background.java
@@ -4,6 +4,8 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+
+
 /**
  * Created by rakion3498 on 11/3/2015.
  */
--- core/src/com/WIPLUU/stressapp/MyGdxGame.java
+++ core/src/com/WIPLUU/stressapp/MyGdxGame.java
@@ -4,8 +4,14 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.ui.Widget ;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+
 
 import javafx.stage.Screen;
 
@@ -14,12 +20,18 @@ public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 	Background test1;
+	TextureAtlas buttonAtlas;
+	Skin buttonSkin;
 	
 	@Override
 	public void create () {
 		img = new Texture("johncena.jpg");
 		test1 = new Background(img);
 		batch = new SpriteBatch();
+		buttonAtlas = new TextureAtlas("buttonAtlas.atlas");
+		buttonSkin = new Skin();
+		buttonSkin.addRegions(buttonAtlas);
+
 	}
 
 	@Override

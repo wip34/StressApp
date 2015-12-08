be6c1d59dbc61a8b70a7ef8102f61def07ab7e6a
wip34 <hanlinip@yahoo.com>
2015-12-08 13:23:38 -0600

    Revert "First Stage"
    This reverts commit 1d3c37f885c290d9f28c17c00c7e45fa4209191d.

--- core/src/com/WIPLUU/stressapp/MyGdxGame.java
+++ core/src/com/WIPLUU/stressapp/MyGdxGame.java
@@ -7,8 +7,6 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.Widget ;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.files.FileHandle;
@@ -19,36 +17,31 @@ import javafx.stage.Screen;
 
 public class MyGdxGame extends ApplicationAdapter {
 
-    SpriteBatch batch;
-    Texture img;
-    Background test1;
-    TextureAtlas buttonAtlas;
-    Skin buttonSkin;
+	SpriteBatch batch;
+	Texture img;
+	Background test1;
+	TextureAtlas buttonAtlas;
+	Skin buttonSkin;
+	
+	@Override
+	public void create () {
+		img = new Texture("johncena.jpg");
+		test1 = new Background(img);
+		batch = new SpriteBatch();
+		buttonAtlas = new TextureAtlas("buttonAtlas.atlas");
+		buttonSkin = new Skin();
+		buttonSkin.addRegions(buttonAtlas);
 
-    @Override
-    public void create() {
-        img = new Texture("johncena.jpg");
-        test1 = new Background(img);
-        batch = new SpriteBatch();
-        buttonAtlas = new TextureAtlas("buttonAtlas.atlas");
-        buttonSkin = new Skin();
-        buttonSkin.addRegions(buttonAtlas);
+	}
 
-    }
-
-    @Override
-    public void render() {
-        Button pika = new Button(buttonSkin);
-        Stage splash = new Stage(   );
-        splash.addActor(pika);
-        Gdx.gl.glClearColor(1, 0, 0, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        batch.begin();
-        test1.draw(batch, Gdx.graphics.getWidth(), Gdx.graphics.getHeight()); //change later
-        splash.draw();
-        batch.end();
-
-    }
-}
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		test1.draw(batch, Gdx.graphics.getWidth(), Gdx.graphics.getHeight()); //change later
+		batch.end();
+		
+	}
 
 

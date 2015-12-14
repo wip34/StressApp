ebd68354c29502521f7bc0a21cbee392dd612efe
AlbertLuu <AlbertLuu42@gmail.com>
2015-12-14 14:40:40 -0600

    Select Box
    Trying Select Box

--- core/src/com/WIPLUU/stressapp/MyGdxGame.java
+++ core/src/com/WIPLUU/stressapp/MyGdxGame.java
@@ -9,6 +9,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
 
+import com.badlogic.gdx.scenes.scene2d.ui.SelectBox;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
@@ -16,38 +17,47 @@ import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 
 
 
-public class MyGdxGame extends ApplicationAdapter {
+public class MyGdxGame extends ApplicationAdapter  {
 
-    SpriteBatch batch;
-    Texture img;
-    Background test1;
-    TextureAtlas buttonAtlas;
-    Skin buttonSkin;
-    Button pewpButton;
+	SpriteBatch batch;
+	Texture img;
+	Background test1;
+	TextureAtlas buttonAtlas;
+	Skin buttonSkin;
+	Button startButton;
+    Stage splash;
+	SelectBox what;
+	@Override
+	public void create() {
+		int[] huh = new int[20];
+		for(int i = 0; i < huh.length;i++)
+		{
+			huh[i] = i;
+		}
+		img = new Texture("johncena.jpg");
+		test1 = new Background(img);
+		batch = new SpriteBatch();
+		buttonAtlas = new TextureAtlas("buttonAtlas/buttonAtlas.atlas");
+		buttonSkin = new Skin();
+		buttonSkin.addRegions(buttonAtlas);
+		//startButton = new Button(buttonSkin.getDrawable("pika big"));
+       // splash.addActor(startButton);
+		what= new SelectBox(buttonSkin);
+		what.setItems(huh);
+		splash.addActor(what);
 
-    @Override
-    public void create() {
-        img = new Texture("johncena.jpg");
-        test1 = new Background(img);
-        batch = new SpriteBatch();
-        buttonAtlas = new TextureAtlas("buttonAtlas/buttonAtlas.atlas");
-        buttonSkin = new Skin();
-        buttonSkin.addRegions(buttonAtlas);
-        pewpButton = new Button(buttonSkin.getDrawable("pika big"));
+	}
 
-    }
+	@Override
+	public void render() {
 
-    @Override
-    public void render() {
-        Stage splash = new Stage();
-        splash.addActor(pewpButton);
 
-        Gdx.gl.glClearColor(1, 0, 0, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        batch.begin();
-        test1.draw(batch, Gdx.graphics.getWidth(), Gdx.graphics.getHeight()); //change later
-        splash.draw();
-        batch.end();
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		test1.draw(batch, Gdx.graphics.getWidth(), Gdx.graphics.getHeight()); //change later
+		splash.draw();
+		batch.end();
 
-    }
+	}
 }
